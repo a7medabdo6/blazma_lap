93cc7349e366f44331c34b2ac353b339ed74d41f
pvox dev <abdulrehmandev388@gmail.com>
2023-03-18 20:34:58 +0200

    f

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 // Set config defaults when creating the instance
 import Axios from "axios";
 export default Axios.create({
-  baseURL: "https://142.93.173.111/api/",
+  baseURL: "http://142.93.173.111/api/",
   withCredentials: true,
 });
